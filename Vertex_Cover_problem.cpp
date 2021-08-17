8d6d6ecd3c08e3b051eacde95c0eef100742c430
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-08-17 19:09:06 +0530

    Update Vertex_Cover_problem.cpp

--- Vertex_Cover_problem.cpp
+++ Vertex_Cover_problem.cpp
@@ -1,4 +1,9 @@
-/* Dynamic programming based program for Vertex Cover problem for
+/* problem : A vertex cover of an undirected graph is a subset of its vertices such that for every edge (u, v) of the graph, either ‘u’ or ‘v’ is in vertex cover. Although the name is Vertex Cover, the set covers all edges of the given graph. 
+The problem to find minimum size vertex cover of a graph is NP complete. But it can be solved in polynomial time for trees. In this post a solution for Binary Tree is discussed. The same solution can be extended for n-ary trees.
+
+For example, consider the following binary tree. The smallest vertex cover is {20, 50, 30} and size of the vertex cover is 3. 
+
+Dynamic programming based program for Vertex Cover problem for
 a Binary Tree */
 #include <stdio.h>
 #include <stdlib.h>
