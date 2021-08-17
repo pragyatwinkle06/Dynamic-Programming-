1800239182fc3242c1ec5ca528f6b10bbcdc58cf
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-08-17 19:12:50 +0530

    Update wildcard.cpp

--- wildcard.cpp
+++ wildcard.cpp
@@ -1,3 +1,15 @@
+/* Given a text and a wildcard pattern, implement wildcard pattern matching algorithm that finds if wildcard pattern is matched with text. The matching should cover the entire text (not partial text).
+The wildcard pattern can include the characters ‘?’ and ‘*’ 
+‘?’ – matches any single character 
+‘*’ – Matches any sequence of characters (including the empty sequence)
+
+For example,
+
+Text = "baaabab",
+Pattern = “*****ba*****ab", output : true
+Pattern = "baaa?ab", output : true
+Pattern = "ba*a?", output : true
+Pattern = "a*ab", output : false   */
 // C++ program to implement wildcard
 // pattern matching algorithm
 #include <bits/stdc++.h>
