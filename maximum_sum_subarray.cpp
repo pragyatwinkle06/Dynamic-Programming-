784d3de04835928146d7fdd7c7e4d36c694b0dd2
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-08-21 22:27:14 +0530

    Update maximum_sum_subarray.cpp

--- maximum_sum_subarray.cpp
+++ maximum_sum_subarray.cpp
@@ -1,5 +1,12 @@
 // Program to find maximum sum subarray
 // in a given 2D array
+/*
+Efficient Approach – 
+Kadane’s algorithm for 1D array can be used to reduce the time complexity to O(n^3). The idea is to fix the left and right columns one by one and find the maximum sum 
+contiguous rows for every left and right column pair. We basically find top and bottom row numbers (which have maximum sum) for every fixed left and right column pair.
+To find the top and bottom row numbers, calculate the sum of elements in every row from left to right and store these sums in an array say temp[]. So temp[i] indicates
+sum of elements from left to right in row i. If we apply Kadane’s 1D algorithm on temp[], and get the maximum sum subarray of temp, this 
+maximum sum would be the maximum possible sum with left and right as boundary columns. To get the overall maximum sum, we compare this sum with the maximum sum so far.*/
 #include <bits/stdc++.h>
 using namespace std;
 
