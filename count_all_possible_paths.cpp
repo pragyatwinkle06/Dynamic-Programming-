0ce88d2214e3c035041d01b2c7f3d59537e36445
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-09-03 11:06:43 +0530

    Update count_all_possible_paths.cpp

--- count_all_possible_paths.cpp
+++ count_all_possible_paths.cpp
@@ -1,5 +1,30 @@
+
+
 // A C++ program to count all possible paths
 // from top left to bottom right
+
+
+/* Count all possible paths from top left to bottom right of a mXn matrix
+Difficulty Level : Easy
+Last Updated : 24 Aug, 2021
+The problem is to count all the possible paths from top left to bottom right of a mXn matrix with
+the constraints that from each cell you can either move only to right or down
+Examples :
+ 
+
+Input :  m = 2, n = 2;
+Output : 2
+There are two paths
+(0, 0) -> (0, 1) -> (1, 1)
+(0, 0) -> (1, 0) -> (1, 1)
+
+Input :  m = 2, n = 3;
+Output : 3
+There are three paths
+(0, 0) -> (0, 1) -> (0, 2) -> (1, 2)
+(0, 0) -> (0, 1) -> (1, 1) -> (1, 2)
+(0, 0) -> (1, 0) -> (1, 1) -> (1, 2)      */
+
 #include <iostream>
 using namespace std;
 
