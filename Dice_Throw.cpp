2e14cd048a4d43113468c8f5a0d48009af7a6f8d
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-09-18 09:16:37 +0530

    Update Dice_Throw.cpp

--- Dice_Throw.cpp
+++ Dice_Throw.cpp
@@ -1,5 +1,5 @@
 // C++ program to find number of ways to get sum 'x' with 'n'
-// dice where every dice has 'm' face
+// dice where every dice has 'm' faces
 #include <iostream>
 #include <string.h>
 using namespace std;
