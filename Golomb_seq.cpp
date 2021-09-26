10a16d3e02b4d386205b628ec1a8a7699f278d4e
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2021-09-26 07:05:37 +0530

    Update Golomb_seq.cpp

--- Golomb_seq.cpp
+++ Golomb_seq.cpp
@@ -1,7 +1,7 @@
 /* Golomb sequence
 
 In mathematics, the Golomb sequence is a non-decreasing integer sequence where n-th term is equal to number of times n appears in the sequence.
-The first few values are 
+The first few values are .....
 1, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 
 Explanation of few terms: 
 Third term is 2, note that three appears 2 times. 
